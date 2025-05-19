b3ac8da371c703d26436ce09281ebc261f9c3ed4
Hannes Mühleisen <hannes@muehleisen.org>
2025-05-19 12:43:15 +0200

    script updates

--- analysis.sql
+++ analysis.sql
@@ -1,24 +1,32 @@
 
-create temporary table old_default as from read_json('2012_default/profile*.json', filename=TRUE) select median(latency)::DECIMAL(3,1) latency_old, (string_split(filename, '-'))[2]::int exp group by exp;
+create temporary table new_default as from read_json('sf1000_2023/profile*.json', filename=TRUE) select median(latency)::DECIMAL(5,1) latency_new, (string_split(filename, '-'))[2]::int exp group by exp;
 
-create temporary table new_default as from read_json('2023_default/profile*.json', filename=TRUE) select median(latency)::DECIMAL(3,1) latency_new, (string_split(filename, '-'))[2]::int exp group by exp;
+create temporary table new_default2 as from read_json('sf1000_2023/profile*.json', filename=TRUE) select *, latency::DECIMAL(5,1) latency_new, (string_split(filename, '-'))[2]::int exp order by exp;
 
-create temporary table old_settings as from read_json('2012_settings/profile*.json', filename=TRUE) select median(latency)::DECIMAL(3,1) latency_old, (string_split(filename, '-'))[2]::int exp group by exp;
+-- from new_default
 
-create temporary table new_settings as from read_json('2023_settings/profile*.json', filename=TRUE) select median(latency)::DECIMAL(3,1) latency_new, (string_split(filename, '-'))[2]::int exp group by exp;
+create temporary table new_default2 as from read_json('sf1000_2012/profile*.json', filename=TRUE) select  latency::DECIMAL(5,1) latency_old, string_split(filename, '-') exp;
+from new_default2 select exp[2]::int query, latency_old latency where exp[3] = '1.json' order by query;
+
+
+
+
+create temporary table old_default as from read_json('sf1000_2012/profile*.json', filename=TRUE) select median(latency)::DECIMAL(5,1) latency_old, (string_split(filename, '-'))[2]::int exp group by exp;
+
 
 
 .mode markdown
 
 
-select 'default settings', geomean(latency_old), geomean(latency_new), geomean(latency_old)/ geomean(latency_new)  from old_default join new_default using (exp);
+select sum(latency_old) sum_old, geomean(latency_old)::DECIMAL(5,1) geomean_old, from old_default;
 
-select 'default settings', exp, latency_old, latency_new, (latency_old/latency_new)::DECIMAL(3,1) speedup from old_default join new_default using (exp) order by exp;
 
-select 'fair settings', geomean(latency_old), geomean(latency_new), geomean(latency_old)/ geomean(latency_new)  from old_settings join new_settings using (exp);
+select exp query, latency_old latency, (latency/60.0)::DECIMAL(5,1) mins from old_default order by exp;
 
-select 'fair settings', exp, latency_old, latency_new, (latency_old/latency_new)::DECIMAL(3,1) speedup from old_settings join new_settings using (exp) order by exp;
 
+select sum(latency_old) sum_old, sum(latency_new) sum_new, geomean(latency_old)::DECIMAL(5,1) geomean_old, geomean(latency_new)::DECIMAL(5,1) geomean_new, (geomean(latency_old)/ geomean(latency_new))::DECIMAL(5,1) speedup from old_default join new_default using (exp);
+
+select exp query, latency_old, latency_new, (latency_old/latency_new)::DECIMAL(4,2) speedup from old_default join new_default using (exp) order by exp;
 
 
 
